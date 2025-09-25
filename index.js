92ee3ea1301e23a2464d5aa37ab9aa0b0048687d
kibetkoech <vickibelangs@gmail.com>
2025-09-26 01:24:16 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+// index.js
+const cats = ["Milo", "Otis", "Garfield"];
+// 1. Destructive functions (these modify the original cats array)
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// 2. Non-destructive functions (these return new arrays, leaving original unchanged)
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
